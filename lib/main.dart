292a196ec0fb9cf7c45ced27474f781f397172ee
ShashanK Kumar Shukla <skshukl00@gmail.com>
2023-08-10 20:42:11 +0530

    dicee project for learning init

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
@@ -20,6 +20,21 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            Container(
+              child: Expanded(
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+            Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
